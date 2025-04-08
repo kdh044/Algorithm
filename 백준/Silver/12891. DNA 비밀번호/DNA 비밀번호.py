da4760210e1e04bdedc98dc98116fe4d199af89c
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-04-08 23:29:18 +0900

    [Silver II] Title: DNA 비밀번호, Time: 496 ms, Memory: 34344 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -1,42 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-# 입력
-n, m = map(int, input().split())
+n , m = map(int,input().split())
 dna = input().strip()
-a, c, g, t = map(int, input().split())
-checkList = [a, c, g, t]
+a,c,t,g = map(int,input().split())
 
-# A, C, G, T 개수 세기 위한 리스트
-def char_to_index(c):
+#자 이제 해야할거
+#슬라이딩 윈도우는 FIFO 큐와같은 부분
+#근데 그전에 문자열과 a,c,t,g의 숫자비교해서 되는지 확인하는 부분 필요함
+#그걸 위해서는 a,c,t,g가 얼마나 카운팅됐는지 알려주는 함수가 필요함
+#인덱스로 보내야겠다 카운팅은 함수를 만들자
+#그리고 나서 슬라이딩 윈도우 알고리즘을 쓰면 되겠다.
+#리스트로 먼저 만들자 빈 리스트를
+
+A_list = [0,0,0,0]
+B_list = [a,c,t,g]
+
+def char_count(c):
     if c == 'A': return 0
-    elif c == 'C': return 1
-    elif c == 'G': return 2
-    elif c == 'T': return 3
-
-# 초기 세팅
-myList = [0, 0, 0, 0]
+    elif c == "C": return 1
+    elif c == "G": return 2
+    else: return 3
+    
 for i in range(m):
-    idx = char_to_index(dna[i])
-    myList[idx] += 1
+    A_list[char_count(dna[i])] += 1  #몰랐던 부분 인덱스를 하나 증가시킴
 
-# 조건 확인 함수
-def is_valid():
+def is_True():
     for i in range(4):
-        if myList[i] < checkList[i]:
+        if A_list[i] < B_list[i]:
             return False
     return True
 
-# 초기 윈도우 검사
-result = 1 if is_valid() else 0
+result = 1 if is_True() else 0
 
-# 윈도우 이동하며 검사
-for i in range(m, n):
-    out_char = dna[i - m]
-    in_char = dna[i]
-    myList[char_to_index(out_char)] -= 1
-    myList[char_to_index(in_char)] += 1
-    if is_valid():
+for i in range(m,n):
+    A_list[char_count(dna[i - m])] -= 1
+    A_list[char_count(dna[i])] += 1
+    if is_True():
         result += 1
-
 print(result)
+    
