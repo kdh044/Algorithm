e033c2d4902be3b870b2132c61923e8662637109
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-04-09 23:14:37 +0900

    [Silver II] Title: 스택 수열, Time: 112 ms, Memory: 39268 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -2,26 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-sequence = [int(input()) for _ in range(n)]
-
+a = [int(input()) for _ in range(n)]
+num = 1
+answer = []
 stack = []
-result = []
-current = 1  # 다음에 push할 수
-
-for target in sequence:
-    while current <= target:
-        stack.append(current)
-        result.append('+')
-        current += 1
-
-    # 현재 target이 스택 맨 위에 있어야 pop 가능
-    if stack and stack[-1] == target:
+for i in range(n):
+    if a[i] >= num:
+        while a[i] >= num:
+            stack.append(num)
+            num += 1
+            answer.append("+")
         stack.pop()
-        result.append('-')
+        answer.append("-")
     else:
-        print("NO")
-        sys.exit(0)
-
-# 정상적으로 종료되었을 경우 출력
-for r in result:
-    print(r)
+        if stack[-1] != a[i]:
+            print("NO")
+            sys.exit(0)
+        answer.append("-")
+        stack.pop()
+print('\n'.join(answer))
