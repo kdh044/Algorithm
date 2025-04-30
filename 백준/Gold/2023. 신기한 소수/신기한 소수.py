b8423a0b9a433c8855ee82bc405b547ac607ccda
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-04-30 19:57:50 +0900

    [Gold V] Title: 신기한 소수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -4,13 +4,10 @@ sys.setrecursionlimit(10000)
 n = int(input())
 
 def isPrime(num):
-    # 2는 소수입니다
     if num == 2:
         return True
-    # 1과 짝수는 소수가 아닙니다 (2 제외)
-    if num < 2 or num % 2 == 0:
+    elif num % 2 == 0 or num < 2:
         return False
-    # 제곱근까지만 체크하면 됩니다
     for i in range(3, int(num**0.5) + 1, 2):
         if num % i == 0:
             return False
@@ -23,8 +20,10 @@ def DFS(number):
         for i in range(1,10):
             if i % 2 == 0:
                 continue
-            if isPrime(number * 10 + i):
-                DFS(number * 10 + i)
+                
+            a = 10 * number + i
+            if isPrime(a):
+                DFS(a)
 DFS(2)
 DFS(3)
 DFS(5)
