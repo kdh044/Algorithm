b7ad4e85dba359fe821b17a3b5044486d09492c6
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-05-08 17:55:43 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35676 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,41 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 sys.setrecursionlimit(10000)
-from collections import deque
+n,m,start = map(int,input().split())
 
-n, m, start = map(int,input().split())
-a = [[] for _ in range(n + 1)]
+a = [[]for _ in range(n+1)]
 
-for _ in range(m):
+for i in range(m):
     s,e = map(int,input().split())
     a[s].append(e)
     a[e].append(s)
 
-for i in range(n + 1):
+for i in range(n+1):
     a[i].sort()
 
-visited = [False] * (n + 1)
+visited = [False] * (n+1)
 
 def DFS(v):
-    print(v, end = ' ')
+    print(v,end = ' ')
     visited[v] = True
     for i in a[v]:
         if not visited[i]:
             DFS(i)
-DFS(start)
-print()
 
 def BFS(v):
     queue = deque()
     queue.append(v)
     visited[v] = True
     while queue:
-        now_Node = queue.popleft()
-        print(now_Node, end = ' ')
-        for i in a[now_Node]:
+        now_node = queue.popleft()
+        print(now_node,end = ' ')
+        for i in a[now_node]:
             if not visited[i]:
                 visited[i] = True
                 queue.append(i)
-                
-visited = [False] * (n + 1)
+        
+DFS(start)
+print()
+visited = [False]*(n+1)
 BFS(start)
