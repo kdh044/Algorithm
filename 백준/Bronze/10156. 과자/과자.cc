d865fc085cfa143279c9fa0de84b075bd5b296d8
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-08-01 16:29:51 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y, z;
+    cin >> x >> y >> z;
+    if (x*y <= z)
+        cout << 0;
+    else
+        cout << (x * y - z);
+    return 0;
+
+}
