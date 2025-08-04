9b59b116ca4ca913855cf81e7672a05bff8c4b3c
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-08-04 16:13:25 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.cc
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -5,7 +5,7 @@ int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int sum = 0;
-    int a[0];
+    int a[5];
     for(int i = 0; i < 5; i++){
          cin >> a[i];
          sum += a[i];
