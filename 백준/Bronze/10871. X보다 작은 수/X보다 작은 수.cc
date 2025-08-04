59601c7157dfe40be9d2e2fbc491d0e3a19aef39
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-08-04 11:14:56 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //파이썬이면 리스트로 정렬한다음에 반복문써서 5보다 작은수 뽑았을텐데...  C++은 구현이힘드네
+    int n, x, a[10000];
+    cin >> n >> x;
+    for(int i=0; i<n; i++) cin >> a[i];
+    for(int j=0; j<n; j++)
+        if (a[j] < x) cout << a[j] << ' '; 
+    return 0;
+}
