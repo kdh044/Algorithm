ecf0714fea56b1216e693cfca62b70e2e5d7d164
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-08-04 17:10:50 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -4,30 +4,21 @@ using namespace std;
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int a[9];
-    int total;
-    int result[7];
+    int a[9] , total , result[7];
     
     for(int i = 0; i < 9; i++) cin >> a[i];
     
-    for(int x = 0; x < 8; x++){
-        total = 0;
-        
-        for(int y = x + 1; y < 9; y++){
+    for(int i = 0; i < 8; i++){
+        for(int j = i + 1; j < 9; j++){
             total = 0;
-            
-            for(int c = 0, i = 0; c < 9; c++) 
-                if(c != x && c != y) 
-                    result[i++] = a[c];
-                    
-            for(int i = 0; i < 7; i++) total += result[i];
-            
-            if(total == 100) break;
-        }
-        if(total == 100) break;
+        
+        for(int c = 0, d = 0; c < 9; c++) if (c != i and c != j) result[d++] = a[c];
+        for(int i = 0; i < 7; i++) total += result[i];
+        if (total == 100) break;
+    }if (total == 100) break;
     }
+    sort(result , result + 7);
+    for(int i = 0; i< 7; i++) cout << result[i] <<"\n";
     
-    sort(result, result + 7);
-    for(int i = 0; i < 7; i++) cout << result[i] << "\n";
+    
 }
