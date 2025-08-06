8d0f71c0994aba5a841c61be68d89731fb3b6504
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-08-06 14:39:49 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int a,b,c,num;
-    int arr[10] = {};
-    cin >> a >> b >> c;
-    num = a * b * c;
-    string str = to_string(num);
-    
-    for(char c : str){
-        arr[c - '0']++;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int A, B, C;
+  cin >> A >> B >> C;
+  int t = A * B * C;
+  int d[10] = {};
+  // baaaaarking dog 식 풀이 형변환 x
+    while(t > 0){
+        d[t % 10]++;
+        t = t/10;
     }
-    
-    for(int i = 0; i < 10; i++){
-        cout << arr[i] << "\n";
+    for(int i = 0; i< 10; i++) cout << d[i] << "\n";
     }
-}
