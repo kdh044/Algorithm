118884ad40cd0300dcbf302bc94ab7b97fe458c2
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-08-07 11:18:44 +0900

    [Silver II] Title: 키로거, Time: 248 ms, Memory: 19384 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.cc
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -1,36 +1,39 @@
+// Authored by : BaaaaaaaaaaarkingDog
+// Co-authored by : -
+// http://boj.kr/49ff74ffdded4e0381138dabbaf8405b
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int n;
-    cin >> n;
-    
-    while(n--){
-        string pwd;
-        cin >> pwd;
-        
-        list<char> L;
-        auto cursor = L.begin();
-        
-        for(auto c : pwd){
-            if(c == '<') {
-                if(cursor != L.begin()) cursor--;
-            }
-            else if(c == '>'){
-                if(cursor != L.end()) cursor++;
-            }
-            else if(c == '-'){
-                if(cursor != L.begin()) {
-                    cursor--;
-                    cursor = L.erase(cursor);
-                }
-            }
-            else L.insert(cursor,c);
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    list<char> L = {};
+    string s;
+    auto p = L.begin();
+
+    cin >> s;
+    for (auto c : s) {
+      if (c == '<') {
+        if (p != L.begin()) p--;
+      }
+      else if (c == '>') {
+        if (p != L.end()) p++;
+      }
+      else if (c == '-') {
+        if (p != L.begin()) {
+          p--;
+          p = L.erase(p);
         }
-       for(auto c : L) cout << c;
-       cout << "\n"; 
+      }
+      else
+        L.insert(p, c);      
     }
+    for (auto c : L) cout << c;
+    cout << '\n';
+  }
 }
