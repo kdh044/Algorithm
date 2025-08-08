d12a4a4bb981a1984b82f7e697e4251af3cd8460
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-08-08 16:44:46 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+        char c;
+        cin >> c;
+
+        cout << int(c);
+  
+}
