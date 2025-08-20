8683a7a5166e364e06d3ba61a36cd9882153ca9a
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-08-20 16:54:06 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+     ios::sync_with_stdio(0);
+     cin.tie(0);
+    
+    int ax,ay,az,bx,by,bz,cx,cy,cz;
+    
+    cin >> ax >> ay >> az;
+    cin >> cx >> cy >> cz;
+    
+    cout << cx - az << ' ' << cy / ay << ' ' << cz - ax;
+    
+}
