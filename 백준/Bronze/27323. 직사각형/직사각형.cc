e7347cdf7bddbfa9199f32393e08d5f19a3684b6
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-09-05 20:08:14 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a, b;
+    cin >> a >> b;
+    int s;
+    s = a * b;
+    cout << s;
+}
