e04a89315485dbb30c6e36b302ae07ddb4f61fc7
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-09-23 16:25:52 +0900

    [Bronze III] Title: 수박 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31868. 수박 게임/수박 게임.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, k;
+    
+    cin >> n >> k;
+    int fruit[n + 2] = {};
+    fruit[1] = k;
+    for(int i = 2; i < n + 1; i++){
+        fruit[i] = fruit[i - 1] / 2;
+    }
+    cout << fruit[n];
+}
