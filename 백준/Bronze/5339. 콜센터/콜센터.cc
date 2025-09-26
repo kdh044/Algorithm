7b1a9c0599b2d4300edb6bf52e139acfa70d8044
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-09-26 10:49:00 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cout << "     /~\\" << '\n';
+    cout << "    ( oo|" << '\n';
+    cout << "    _\\=/_" << '\n';
+    cout << "   /  _  \\" << '\n';
+    cout << "  //|/.\\|\\\\" << '\n';
+    cout << " ||  \\ /  ||" << '\n';
+    cout << "============" << '\n';
+    cout << "|          |" << '\n';
+    cout << "|          |" << '\n';
+    cout << "|          |" << '\n';
+}
