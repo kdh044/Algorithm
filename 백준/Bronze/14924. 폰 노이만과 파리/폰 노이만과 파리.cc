ae5c8782ed3b1e415a3353229c5314e82d50ae67
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-10-05 18:04:32 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int s,d,t;
+    cin >> s >> d >> t;
+    cout << t * d / (2 * s); 
+}
