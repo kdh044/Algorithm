a46cc9d9ea20ef40677a30ba534003cf4ffcd6dd
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-10-14 22:08:56 +0900

    [Silver III] Title: N과 M (3), Time: 328 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int arr[10];
+
+void check(int k){
+    if(k == m){
+        for(int i = 0; i < m; i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    } 
+    
+    int st = 1;
+    for(int i = st; i <= n; i++){
+        arr[k] = i;
+        check(k + 1);
+    }
+}
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    check(0);
+}
