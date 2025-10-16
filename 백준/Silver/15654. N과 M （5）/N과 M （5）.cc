f79f15637fb14f7f17de3a43d918108d5c351410
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-10-17 00:59:30 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -1,34 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int n, m;
-int num[10];        // 입력 배열
-int arr[10];        // 현재 순열 저장
-bool used[10];      // 사용 여부 체크
+int arr[10];
+int num[10];
+bool isused[10];
 
-void dfs(int x) {
-    if (x == m) {
-        for (int i = 0; i < m; i++) cout << arr[i] << " ";
+void func(int k){
+    if(k == m){
+        for(int i = 0; i < m; i++){
+            cout << num[arr[i]] << ' ';
+        }
         cout << "\n";
         return;
     }
-
-    for (int i = 0; i < n; i++) {
-        if (used[i]) continue; // 이미 사용한 수는 건너뜀
-        used[i] = true;
-        arr[x] = num[i];
-        dfs(x + 1);
-        used[i] = false;      // 재귀 종료 후 되돌리기
+    for(int i = 0; i < n; i++){
+        if(!isused[i]){
+            arr[k] = i;
+            isused[i] = 1;
+            func(k + 1);
+            isused[i] = 0;
+        }
     }
+    
 }
-
-int main() {
+int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin >> n >> m;
-    for (int i = 0; i < n; i++) cin >> num[i];
-
-    sort(num, num + n);  // 사전 순 출력을 위해 정렬
-    dfs(0);
+    for(int i = 0; i < n; i++){
+        cin >> num[i];
+    }
+    sort(num, num + n);
+    func(0);
 }
