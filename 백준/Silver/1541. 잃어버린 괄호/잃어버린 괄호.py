a84659fb22a9eb2974d4c60e98f3258e25d58a46
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-11-28 17:14:09 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,10 +1,7 @@
 a = input().split("-")
-total = 0
 
 total = sum(map(int,a[0].split("+")))
 
 for p in a[1:]:
-    total -= sum(map(int, p.split('+')))
-
+    total -= sum(map(int,p.split("+")))
 print(total)
-
