b912fbd2b91ed066c59ae032a86e4eb8eee02e56
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-11-29 20:38:06 +0900

    [Gold V] Title: 거의 소수, Time: 3048 ms, Memory: 112664 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.py
+++ 백준/Gold/1456. 거의 소수/거의 소수.py
@@ -1,24 +1,23 @@
 import math
 
-low, high = map(int, input().split())
-
-limit = int(math.sqrt(high)) + 1
+low, high = map(int,input().split())
+limit = int(math.sqrt(high) + 1)
 prime = [True] * (limit + 1)
 prime[0] = prime[1] = False
 
-# 에라토스테네스
-for i in range(2, int(limit**0.5)+1):
+for i in range(2, int(math.sqrt(limit) + 1)):
     if prime[i]:
-        for j in range(i*i, limit+1, i):
+        for j in range(i * i, limit + 1, i):
             prime[j] = False
 
 count = 0
-for p in range(2, limit+1):
-    if prime[p]:
-        x = p * p
+
+for i in range(2, limit + 1):
+    if prime[i]:
+        x = i * i
         while x <= high:
             if x >= low:
                 count += 1
-            x *= p
-
+            x *= i
+            
 print(count)
