646cc7678e3f0dc670285c6467e3e0c2c4fd48b6
김동훈 <147026620+kdh044@users.noreply.github.com>
2025-12-08 22:45:35 +0900

    [Bronze IV] Title: 2, 4, 6, 8, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/34509. 2， 4， 6， 8/2， 4， 6， 8.py
+++ 백준/Bronze/34509. 2， 4， 6， 8/2， 4， 6， 8.py
@@ -1,5 +1,6 @@
-for i in range(10,100):
-    i = str(i)
-    b = i[:-1]
-    if int(b) % 4 == 0 and "8" not in i and int(i) % 6 == 0:
-        print(int(i))
+for n in range(10, 100):
+    s = str(n)      
+    tens = int(s[0])
+
+    if tens % 4 == 0 and "8" not in s and n % 6 == 0:
+        print(n)
