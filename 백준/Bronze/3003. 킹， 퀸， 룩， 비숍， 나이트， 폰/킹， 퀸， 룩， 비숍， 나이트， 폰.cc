1040c23b6e72542747a5b9eecb59320ce10d73dc
김동훈 <147026620+kdh044@users.noreply.github.com>
2026-04-13 09:53:38 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[6], b[6] = {1,1,2,2,2,8};
+    for(int i = 0; i < 6; i++){
+        cin >> a[i];
+            cout << b[i] - a[i] << " ";
+    }
+}
