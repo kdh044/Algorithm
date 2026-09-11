03cb09a2c481f26710314fedbca80b7b6cc80e9d
김동훈 <147026620+kdh044@users.noreply.github.com>
2026-09-11 17:17:03 +0900

    [level 2] Title: 전화번호 목록, Time: 386.77 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -4,7 +4,7 @@ def solution(phone_book):
     for number in phone_set:
         for i in range(1,len(number)):
             number_split = number[:i]
-            
+
             if number_split in phone_set:
                 return False
     return True
