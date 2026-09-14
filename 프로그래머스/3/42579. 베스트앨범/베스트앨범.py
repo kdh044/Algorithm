d963fc79ee273cb3ca67f6585cd838199058ed27
김동훈 <147026620+kdh044@users.noreply.github.com>
2026-09-14 14:19:04 +0900

    [level 3] Title: 베스트앨범, Time: 0.05 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,31 +1,25 @@
-def solution(genres, plays):
-    answer = []
-
-    dic1 = {}
-    dic2 = {}
-
-    for i, (g, p) in enumerate(zip(genres, plays)):
-
-        # 장르별 노래 저장
-        if g not in dic1:
-            dic1[g] = [(i, p)]
-        else:
-            dic1[g].append((i, p))
-
-        # 장르별 총 재생수
-        if g not in dic2:
-            dic2[g] = p
-        else:
-            dic2[g] += p
-
-    # 총 재생수가 많은 장르부터
-    for k, v in sorted(dic2.items(), key=lambda x: x[1], reverse=True):
-
+def solution(genres, plays): 
+    answer = [] 
+     
+    dic1 = {} 
+    dic2 = {} 
+ 
+    for i, (g, p) in enumerate(zip(genres, plays)): 
+        if g not in dic1: 
+            dic1[g] = [(i, p)] 
+        else: 
+            dic1[g].append((i, p)) 
+         
+        if g not in dic2: 
+            dic2[g] = p 
+        else: 
+            dic2[g] += p 
+ 
+    for k, v in sorted(dic2.items(), key=lambda x: x[1], reverse=True): 
         # 재생수가 많은 노래부터, 같으면 고유번호가 작은 순서
-        songs = sorted(dic1[k], key=lambda x: (-x[1], x[0]))
-
-        # 최대 2개
-        for i, p in songs[:2]:
+        songs = sorted(dic1[k], key=lambda x: (-x[1], x[0]))   
+         
+        for i, p in songs[:2]: 
             answer.append(i)
-
+            
     return answer
