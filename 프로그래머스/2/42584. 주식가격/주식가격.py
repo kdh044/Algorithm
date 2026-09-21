37ebd1c6d045fbc0a60d1191045e237f861cd927
김동훈 <147026620+kdh044@users.noreply.github.com>
2026-09-21 14:03:48 +0900

    [level 2] Title: 주식가격, Time: 15.71 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,18 @@
 def solution(prices):
-    answer = []
-    for i in range(len(prices)):
-        count = 0
-        
-        for j in range(i + 1, (len(prices))):
-            count += 1
-            
-            if prices[i] > prices[j]:
-                break
-                
-        answer.append(count)
-            
+    n = len(prices)
+    answer = [0] * n
+    stack = []
+
+    for i in range(n):
+
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+
+        stack.append(i)
+
+    while stack:
+        j = stack.pop()
+        answer[j] = n - 1 - j
+
     return answer
