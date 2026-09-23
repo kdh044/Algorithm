5a86bdb4f9a11a6e62bf907b75cbbadb7f649fe4
김동훈 <147026620+kdh044@users.noreply.github.com>
2026-09-23 16:44:27 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,5 @@
 def solution(array, commands):
     answer = []
-    for command in commands:
-        temp = []
-        i = command[0]
-        j = command[1]
-        k = command[-1]
-        temp = array[i - 1:j]
-        temp.sort()
-        answer.append(temp[k - 1])
+    for com in commands:
+        answer.append(sorted(array[com[0] - 1: com[1]])[com[2] - 1])
     return answer
